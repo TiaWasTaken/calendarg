9d8cf45c72587273f73cf42e6b71e2ce41fd904d
TiaWasTaken <arganetto.mattia@gmail.com>
2024-10-08 21:21:11 +0200

    created prisma config and linking to supabase

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,8 +1,11 @@
 import NextAuth from 'next-auth';
 import Github from 'next-auth/providers/github'
 import Google from 'next-auth/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import prisma from './db'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   // The providers for auth in the calendar
+  adapter: PrismaAdapter(prisma),
   providers:[Github, Google],
 });
