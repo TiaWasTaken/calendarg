edada5876b9459e8265c2d145b7731dc1ddd47dc
TiaWasTaken <arganetto.mattia@gmail.com>
2024-10-11 09:41:27 +0200

    added the possibility to log in with 2 accounts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,11 +1,22 @@
 import NextAuth from 'next-auth';
-import Github from 'next-auth/providers/github'
-import Google from 'next-auth/providers/google'
-import { PrismaAdapter } from '@auth/prisma-adapter'
-import prisma from './db'
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from './db';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  // The providers for auth in the calendar
   adapter: PrismaAdapter(prisma),
-  providers:[Github, Google],
+  providers: [
+    Github({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,  // Enable account linking for GitHub
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      allowDangerousEmailAccountLinking: true,  // Enable account linking for GitHub
+    }),
+  ],
 });
+
