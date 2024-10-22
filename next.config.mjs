60a0790a4b101acb52f3b2f2d17e385d8ed99f47
TiaWasTaken <arganetto.mattia@gmail.com>
2024-10-22 09:13:42 +0200

    changed imeges.domains due to deprecation

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['utfs.io', 'lh3.googleusercontent.com'], // Add the domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 };
 
